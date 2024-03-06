b43c4e173529bf27a3781ae4ff8a847eb73023f8
pizza6inch <126549982+pizza6inch@users.noreply.github.com>
2024-03-06 10:59:54 +0800

    comment

--- Frontend-Intern-Homework/src/component/Modal/AddModal.jsx
+++ Frontend-Intern-Homework/src/component/Modal/AddModal.jsx
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react"; // importing necessary modules
 import Modal from "react-modal";
 import "./Modal.css";
+
 const customStyles = {
   content: {
     top: "25%",
@@ -9,33 +10,32 @@ const customStyles = {
     bottom: "auto",
     width: "50%",
     height: "50%",
-    color: "black",
+    color: "black", // styles for the modal
   },
 };
 
 function AddModal(props) {
-  useEffect(() => {
+  useEffect(() => { // component did mount
     const rootElement = document.getElementById("root");
     Modal.setAppElement(rootElement);
   }, []);
 
   function closeModal() {
-    props.setIsOpen(false);
+    props.setIsOpen(false); // close the modal
   }
 
   async function AddIssue() {
     console.log("AddIssue");
-    await fetch("http://localhost:4000/AddIssue", {
-      //要改成新增的API
+    await fetch("http://localhost:4000/AddIssue", { // API call to add issue
       method: "POST",
       headers: {
-        Authorization: localStorage.getItem("accessToken"),
-        title: document.querySelector(".title-input").value,
-        body: document.querySelector(".content-input").value,
+        Authorization: localStorage.getItem("accessToken"), // Authorization token
+        title: document.querySelector(".title-input").value, // getting title from input
+        body: document.querySelector(".content-input").value, // getting content from input
       },
     });
     closeModal();
-    window.location.reload();
+    window.location.reload(); // reload the page after adding issue
   }
 
   return (
@@ -47,24 +47,24 @@ function AddModal(props) {
         contentLabel="Example Modal"
       >
         <div className="title-wrapper">
-          <h2>Title:</h2>
+          <h2>Title:</h2> {/* label for title input */}
           <input className="title-input" type="text">
             {}
-          </input>
+          </input> {/* input field for title */}
         </div>
         <div className="content-wrapper">
-          <h2>Content:</h2>
+          <h2>Content:</h2> {/* label for content input */}
           <input className="content-input" type="text">
             {}
-          </input>
+          </input> {/* input field for content */}
         </div>
         <div className="button-wrapper">
           <button className="confirm-button" type="button" onClick={AddIssue}>
             Confirm
-          </button>
+          </button> {/* button to confirm adding issue */}
           <button className="cancel-button" type="button" onClick={closeModal}>
             Cancel
-          </button>
+          </button> {/* button to cancel adding issue */}
         </div>
       </Modal>
     </>
